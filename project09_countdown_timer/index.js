fe4151bc8f7264c4ac32ace07cb506057b824824
rmusamamukhtar <114367301+RMUSAMA@users.noreply.github.com>
2023-02-06 02:04:34 +0500

    Add files via upload

---
+++ project09_countdown_timer/index.js
@@ -0,0 +1,44 @@
+import chalk from "chalk";
+import inquirer from "inquirer";
+async function welcome() {
+    console.log(chalk.whiteBright(`            -----------------`));
+    console.log(chalk.yellowBright(`            COUNT DOWN TIMER `));
+    console.log(`            -----------------`);
+    console.log(chalk.whiteBright(`STOP-WATCH:
+   `));
+}
+async function question() {
+    const answers = await inquirer.prompt([{
+            type: "input",
+            name: "question1",
+            message: "For how much time you want to run your stop watch \n Enter your time in seconds",
+        }]);
+    let countdown = answers.question1;
+    const timer = setInterval(() => {
+        countdown--;
+        process.stdout.clearLine(0);
+        process.stdout.cursorTo(0);
+        process.stdout.write(" Seconds Remaining :  " + countdown);
+        if (countdown <= 0) {
+            console.log("\n Time's up!");
+            clearInterval(timer);
+        }
+    }, 1000);
+}
+// let endDate = new Date("2023-02-05");
+// const timer = setInterval(() => {
+//   let currentDate = new Date();
+//   let difference = endDate.getTime() - currentDate.getTime();
+//   let remainingSeconds = Math.floor(difference / 1000);
+//   if (remainingSeconds <= 0) {
+//     console.clear();
+//     console.log("Time's up!");
+//     clearInterval(timer);
+//   } else {
+//     process.stdout.clearLine(0);
+//     process.stdout.cursorTo(0);
+//     process.stdout.write( " Seconds Remaining" + remainingSeconds );
+//   }
+// }, 1000);
+await welcome();
+await question();
