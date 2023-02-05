476ad4be6110e5362541da1edc2b8e01d411ec1a
rmusamamukhtar <114367301+RMUSAMA@users.noreply.github.com>
2023-02-06 02:06:55 +0500

    Add files via upload

---
+++ project10_oop/index.js
@@ -0,0 +1,43 @@
+import chalk from "chalk";
+import inquirer from "inquirer";
+async function welcome() {
+    console.log(chalk.whiteBright(`            --------------------------`));
+    console.log(chalk.yellowBright(`            OBJECT ORIENTED PROGRAMING`));
+    console.log(`            --------------------------`);
+    console.log(chalk.whiteBright(`OOP:    
+     `));
+}
+class Person {
+    constructor(personality = "Mystery") {
+        this.personality = personality;
+    }
+    personality;
+}
+async function question1() {
+    const answer = await inquirer.prompt([{
+            type: "input",
+            name: "question_1",
+            message: "Type 1 if you want to talk to others and type 2 if you would rather keep to yourself \n",
+            // validate: function(input) {
+            //     if ( input == false) {
+            //       return "Input cannot be a string";
+            //     }
+            //     return true;}
+        }]);
+    if (answer.question_1 == 1) {
+        const pr = new Person("Extrovert");
+        console.table(pr);
+    }
+    else if (answer.question_1 == 2) {
+        const pr = new Person("Introvert");
+        console.table(pr);
+    }
+    else {
+        const pr = new Person();
+        console.log(`You are still a ${pr.personality}`);
+    }
+}
+class Student extends Person {
+}
+await welcome();
+await question1();
